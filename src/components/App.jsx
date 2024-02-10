385e4a6faaf08650730160cd0bc0af337b2c3315
Daniil <negativdaniil4@gmail.com>
2024-02-10 13:00:48 +0200

    TodoList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { nanoid } from "nanoid";
 import { TodoList } from "./TodoList/TodoList";
 import initialTodos from '../todo.json'
 import { TodoEditor } from "./TodoEditor/TodoEditor";
-import { Modal } from "../Modal/Modal";
+// import { Modal } from "../Modal/Modal";
 
 export class App extends Component{
 
@@ -49,11 +49,11 @@ export class App extends Component{
   render(){
     return(
       <>
-      <button type="button" onClick={this.toggleModal}>Відкрити модалку</button>
+      {/* <button type="button" onClick={this.toggleModal}>Відкрити модалку</button>
         {this.state.shovModal && <Modal>
           <button type="button" onClick={this.toggleModal}>Закрити</button>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-        </Modal>}
+        </Modal>} */}
 
         <TodoEditor addToDo={this.addToDo}/>
         <TodoList toDos={this.state.toDos} onDelete={this.deleteToDo} onToggleCompleted={this.toggleCompleted}/>
