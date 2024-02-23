177da4e5a07f6c5340f2585277bd62c7ea84ecfa
Daniil <negativdaniil4@gmail.com>
2024-02-24 01:32:38 +0200

    Update Filter.styled.jsx

--- src/components/Filter/Filter.styled.jsx
+++ src/components/Filter/Filter.styled.jsx
@@ -1,8 +1,6 @@
 import styled from "styled-components";
 
-export const FilterContainer = styled.div`
-
-`
+export const FilterContainer = styled.div``
 
 export const Input = styled.input`
     width: 300px;
